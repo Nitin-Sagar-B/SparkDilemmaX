d1a2eadae93e60fb64eec32fdc78a10ee5873052
Nitin Sagar <100074728+Nitin-Sagar-B@users.noreply.github.com>
2024-06-17 01:50:21 +0530

    Updated final

--- sapp.py
+++ sapp.py
@@ -70,10 +70,10 @@ def main():
     state = st.session_state.get("state", State.INITIAL)
 
     if state == State.INITIAL:
-    # Generate Dilemma Button
-    if st.button('Generate Dilemma'):
-        st.session_state.state = State.GENERATE_CLICKED
-        st.info('Generating new dilemma...click on the generate button again to view')
+        # Generate Dilemma Button
+        if st.button('Generate Dilemma'):
+            st.session_state.state = State.GENERATE_CLICKED
+            st.info('Generating new dilemma...click on the generate button again to view')
     
     elif state == State.GENERATE_CLICKED:
         # Show generated dilemma
